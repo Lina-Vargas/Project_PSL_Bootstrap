7d64b0721e18ad5fbe72d7e379380a0876fce455
socratess <spalaciosm@unal.edu.co>
2017-05-07 01:17:15 -0500

    modificado el ininiar sesion

--- application/controllers/Sesion.php
+++ application/controllers/Sesion.php
@@ -3,22 +3,83 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sesion extends CI_Controller {
 
-function __construct()
-	{
-		parent::__construct();
-		$this->load->model('Usuario_model');
-		
-	}
+
+function __construct(){
+parent:: __construct();
+$this->load->helper('form');
+$this->load->model('Usuario');
+$this->load->helper('url');
+$this->load->library('form_validation');
+}
 
 public function iniciarSesion()
 	{
-		$datos = array('titulo' => "Inicio  Sesión");
+			$mensajes['mensaje'] ="";
+	$datos = array('titulo' => "Inicio  Sesión");
 		$this->load->view('Vista_basica/header', $datos);
-		$this->load->view('InicioSesion');
+		$this->load->view('InicioSesion',$mensajes);
 		$this->load->view('Vista_basica/footer');
 	}
+function revisardatos(){
+
+    $password=$this->input->post('clave');
+
+	$email=$this->input->post('correo');
+
+if($this->input->post()){
+	 		
+	 $this->form_validation->set_rules('correo','Correo electrónico/','required|valid_email');
+	 $this->form_validation->set_rules('clave','Clave','required');
+        
+
+
+if($this->form_validation->run()==True){
+	 	  $Valida=$this->Usuario->revisarcorreo($email);
+
+	 	  $Validas=$this->Usuario->revisarclave($password);
+	 	  
+	 	  if(empty($Valida)){
+          	$mensajes['mensaje'] = "No esta registrado, por favor registrarse,antes revisar el Correo electrónico ingresado";
+          	$datos = array('titulo' => "Inicio  Sesión");
+		$this->load->view('Vista_basica/header', $datos);
+		$this->load->view('InicioSesion',$mensajes);
+		$this->load->view('Vista_basica/footer');
+      } else{
+      	if (empty($Validas)){
+          $mensajes['mensaje'] = "No esta registrado, por favor registrarse,antes revisar la contraseña ingresada";
+          	$datos = array('titulo' => "Inicio  Sesión");
+		$this->load->view('Vista_basica/header', $datos);
+		$this->load->view('InicioSesion',$mensajes);
+		$this->load->view('Vista_basica/footer');
+      	}
+      
+
+else{
+          redirect('plan');
+      }
+  }
+
+} else {
+	$mensajes['mensaje'] ="";
+	$datos = array('titulo' => "Inicio  Sesión");
+		$this->load->view('Vista_basica/header', $datos);
+		$this->load->view('InicioSesion',$mensajes);
+		$this->load->view('Vista_basica/footer');
+}
+}
+else{
+	$mensajes['mensaje'] ="";
+	$datos = array('titulo' => "Inicio  Sesión");
+		$this->load->view('Vista_basica/header', $datos);
+		$this->load->view('InicioSesion',$mensajes);
+		$this->load->view('Vista_basica/footer');
+}
+
+}
 
 
 
 
-}	
+}
+
+?>	
--- application/views/InicioSesion.php
+++ application/views/InicioSesion.php
@@ -1,21 +1,38 @@
+<?= form_open("/Sesion/revisardatos")?>
  <div class="container" style="margin-top: 90px;">
         <div class="card card-container">
             <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
             <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
             <p id="profile-name" class="profile-name-card"></p>
-            <form method="post" class="form-signin" action="<?php echo base_url("/Sesion/iniciarSesion"); ?>">
+           
+            <?php
+     $correo= array('name'=>'correo', 'class' => 'form-control', 'value' => set_value('correo'));
+     $clave= array('name'=>'clave', 'class' => 'form-control', 'value' => set_value('clave'));
+     
+?>
+            
                 <span id="reauth-email" class="reauth-email"></span>
-                <input type="email" id="correo" class="form-control" placeholder="Correo Electrónico" required autofocus>
-                <input type="password" id="clave" class="form-control" placeholder="Contraseña" required>
+                <a><?= $mensaje ?></a><br>
+                
+
+  <?= form_label('Correo Electrónico/Email:','correo')?>
+<?=form_input($correo)?><?php echo form_error('correo')?>
+<br><br>
+<?= form_label('Contraseña/clave:','clave')?>
+<?=form_input($clave)?><?php echo form_error('clave')?>
+<br><br>
+
+
                 <div id="remember" class="checkbox">
                     <label>
                         <input type="checkbox" value="remember-me"> Recordar Usuario
                     </label>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Entrar</button>
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Ingresar</button>
             </form><!-- /form -->
             <a href="#" class="forgot-password">
                 Olvidaste la clave?
             </a>
         </div><!-- /card-container -->
   </div><!-- /container -->
+  <?= form_close()?>
