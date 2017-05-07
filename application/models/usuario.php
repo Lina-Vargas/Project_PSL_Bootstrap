ad1e6eb2208a79b8086089d9651152f9035fdd63
socratess <spalaciosm@unal.edu.co>
2017-05-07 01:22:57 -0500

    modificado el modelo usuario

---
+++ application/models/usuario.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') or exit ('No Direct Script Access Allowed');
+
+class Usuario extends CI_Model{
+
+function __construct(){
+ parent:: __construct();
+  $this->load->database();
+}
+
+function revisarcorreo($email){
+$query = $this->db->where('correo',$email);
+$query = $this->db->get('usuario');
+return $query->result();
+
+}
+
+
+function revisarclave($password){
+$queryy = $this->db->where('clave',$password);
+$queryy = $this->db->get('usuario');
+return $queryy->result();
+
+
+}
+function crearusuario($datosusuario){
+
+$this->db->insert('usuario',array(
+		'id'=>$datosusuario['id'],  
+	  	'nombres'=>$datosusuario['nombres'],  
+	  	'apellidos'=>$datosusuario['apellidos'],
+	  	'telefono'=>$datosusuario['telefono'],
+        'celular'=>$datosusuario['celular'],
+	  	'correo'=>$datosusuario['correo'],  
+        'direccion'=>$datosusuario['direccion'],  
+        'clave'=>$datosusuario['clave'] ));
+
+
+}
+
+}
+?>
